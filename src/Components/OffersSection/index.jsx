ad136a60afa3c4349afe9b8be158a5a042fee903
Matheus Teixeira <matheustb12@hotmail.com>
2023-10-31 13:59:03 -0300

    Feat: Estilização da HomePage

---
+++ src/Components/OffersSection/index.jsx
@@ -0,0 +1,15 @@
+import OffersImg from "../../assets/OffersIMG.png"
+import styles from "./styles.module.scss"
+
+export const OffersSection = () => {
+    
+    return(
+        <div className={styles.offers__container}>
+            <img src={OffersImg} alt="" className={styles.offers__img} />
+            <div>
+                <h2>KENZIE FASHION STORE</h2>
+                <button>CONFIRA MAIS</button>
+            </div>
+        </div>
+    )
+}
--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,3 +1,4 @@
+import { OffersSection } from "../../Components/OffersSection"
 import { ProductList } from "../../Components/ProductsList"
 import { TemplatePageAuthenticated } from "../../Components/Templates/TemplatePageAuthenticated"
 import styles from "./styles.module.scss"
@@ -6,7 +7,7 @@ export const HomePage = () => {
     return(
         <>
             <TemplatePageAuthenticated>
-                <p>NOVIDADES</p>
+                <OffersSection/>
                 <h2 className={styles.title_2}>PRODUTOS EM DESTAQUE</h2>
                 <ProductList/>
             </TemplatePageAuthenticated>
